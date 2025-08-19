f4953d7d3ab97d86f135bab5f8862a7a7a81e16c
Nazar Havronskyi <nhavronskyi@gmail.com>
2025-08-19 13:13:18 +0200

    init

---
+++ src/main/java/com/nhavronskyi/prostolearn/ProstoLearnApplication.java
@@ -0,0 +1,13 @@
+package com.nhavronskyi.prostolearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProstoLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProstoLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhavronskyi/prostolearn/ProstoLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhavronskyi.prostolearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProstoLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
